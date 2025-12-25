e6898630b7f7cd2e88334abfa0f8c04a8e37da04
Ashera Cordova <ashera.cordova@gmail.com>
2025-12-25 09:08:33 +0530

    Added setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/
+export PATH=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/:$PATH
+export PATH=/Users/ramm/Documents/JAVA/apache-maven-3.9.11/bin/:$PATH
+export PROTOBUF_ROOT_DIR=/usr/local/opt/protobuf
