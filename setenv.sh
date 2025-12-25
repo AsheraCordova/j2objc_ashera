29f754101325452470947a94d0eb4625b87543c5
Ashera Cordova <ashera.cordova@gmail.com>
2025-12-25 12:03:48 +0530

    Updated to inckude ant path

--- setenv.sh
+++ setenv.sh
@@ -2,3 +2,4 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/
 export PATH=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/:$PATH
 export PATH=/Users/ramm/Documents/JAVA/apache-maven-3.9.11/bin/:$PATH
 export PROTOBUF_ROOT_DIR=/usr/local/opt/protobuf
+export PATH=/Users/ramm/Documents/JAVA/apache-ant-1.10.14/bin/:$PATH
